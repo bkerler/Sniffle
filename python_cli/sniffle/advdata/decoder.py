bbd69a1ea52d8a12935f00e016d2db8234f10136
alphafox02 <44436101+alphafox02@users.noreply.github.com>
2024-06-19 07:35:47 -0400

    Update decoder.py

--- python_cli/sniffle/advdata/decoder.py
+++ python_cli/sniffle/advdata/decoder.py
@@ -2,27 +2,29 @@
 # Copyright (c) 2024, NCC Group plc
 # Released as open source under GPLv3
 
+# decode.py
+
 from struct import unpack
 from .ad_types import *
 from .msd_apple import AppleMSDRecord
 from .msd_microsoft import MicrosoftMSDRecord
-from .msd_remoteid import RemoteIDRecord
+from .msd_remoteid import parse_advertising_data
 
+# Dictionary mapping company identifiers to corresponding MSD record classes
 company_msd_decoders = {
     0x0006: MicrosoftMSDRecord,
     0x004C: AppleMSDRecord
 }
 
+# Function to decode manufacturer specific data
 def decode_msd(data_type: int, data: bytes):
     company, = unpack("<H", data[:2])
-    if company == 0x004C:  # Apple's company identifier
-        return AppleMSDRecord(data_type, data)
-    elif company == 0x0006:  # Microsoft's company identifier
-        return MicrosoftMSDRecord(data_type, data)
+    if company in company_msd_decoders:
+        return company_msd_decoders[company](data_type, data)
     else:
         return ManufacturerSpecificDataRecord(data_type, data)
 
-# https://bitbucket.org/bluetooth-SIG/public/src/main/assigned_numbers/core/ad_types.yaml
+# Dictionary mapping advertising data types to their respective classes or decoding functions
 ad_type_classes = {
     0x01: FlagsRecord,
     0x02: ServiceList16Record,
@@ -37,44 +39,16 @@ ad_type_classes = {
     0x16: ServiceData16Record,
     0x20: ServiceData32Record,
     0x21: ServiceData128Record,
-    0xFF: decode_msd
+    0xFF: parse_advertising_data  # Mapping 0xFF to the Remote ID decoding function
 }
 
-def decode_remote_id(data_type: int, data: bytes):
-    # Implement your Remote ID decoding logic here
-    service, = unpack('<H', data[:2])
-    service_data = data[2:]
-
-    message_counter = service_data[0]
-    messages = []
-
-    # Process the message pack
-    message_size = service_data[2]
-    message_quantity = service_data[3]
-    offset = 4
-
-    for _ in range(message_quantity):
-        message_type = service_data[offset] >> 4
-        message_version = service_data[offset] & 0x0F
-        message_data = service_data[offset + 1: offset + 1 + message_size]
-        messages.append({
-            "type": message_type,
-            "version": message_version,
-            "data": message_data
-        })
-        offset += 1 + message_size
-
-    return RemoteIDRecord(data_type, data, message_counter, messages)  # Replace with appropriate class
-
-
-def record_from_type_data(data_type: int, data: bytes):
-    if data_type in ad_type_classes:
-        try:
-            return ad_type_classes[data_type](data_type, data)
-        except:
-            return AdvDataRecord(data_type, data, malformed=True)
+# Function to decode remote ID data
+def parse_advertising_data(data):
+    # Check if it's Remote ID data (0xFF service data)
+    if data[0] == 0xFF:
+        return decode_remote_id(data)
     else:
-        return AdvDataRecord(data_type, data)
+        return None  # Return None if not Remote ID data
 
 def decode_adv_data(data):
     records = []
